68a2b3a3b2d48c000ed79b03a6207e26296b1852
Harsh Maur <harsh@comparemunafa.com>
2017-01-09 18:51:32 +0530

    first release

---
+++ index.js
@@ -0,0 +1,29 @@
+var ClevertapReact = {
+
+  initialize(accountId) {
+    window.clevertap = { event: [], profile: [], account: [], onUserLogin: [], notifications: [] };
+    window.clevertap.account.push({ 'id': accountId });
+    (function () {
+      let wzrk = document.createElement('script');
+      wzrk.type = 'text/javascript';
+      wzrk.async = true;
+      wzrk.src = ('https:' == document.location.protocol ? 'https://d2r1yp2w7bby2u.cloudfront.net' : 'http://static.clevertap.com') + '/js/a.js';
+      let s = document.getElementsByTagName('script')[0];
+      s.parentNode.insertBefore(wzrk, s);
+    })();
+  },
+
+  event(name, payload = {}) {
+    window.clevertap.event.push(name, payload);
+  },
+
+  profile(payload) {
+    window.clevertap.profile.push(payload);
+  },
+
+  logout() {
+    window.clevertap.logout();
+  }
+};
+
+export default ClevertapReact;
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          'babel-loader',
+        ],
+        exclude: /node_modules/
+      }
+    ],
+  },
+
+}
